142b866142dae8c3b56ace2b51f2e2974615c1c7
Hemant Karekar <hemant.v.karekar@gmail.com>
2024-10-31 09:03:01 +0530

    Change Config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/company-products/smart-hrms/';
+$config['base_url'] = 'http://localhost/products/smart-hrms/';
 
 /*
 |--------------------------------------------------------------------------
