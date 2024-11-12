cc30b5f1984e8afd020ff41def066162d6675026
Hemant Karekar <hemant.v.karekar@gmail.com>
2024-11-12 08:21:33 +0530

    Dashboard Data Sort

--- application/views/main/dashboard/employee/index.php
+++ application/views/main/dashboard/employee/index.php
@@ -14,6 +14,28 @@
     <div>
       <h4 class="mb-3 mb-md-0">Welcome to the Basic Dashboard</h4>
     </div>
+    <div class="">
+      <?= form_open() ?>
+      <div class="row">
+        <div class="col-auto">
+          <select name="" id="select2Period" class="form-control-sm">
+            <option value="">-- Select Duration --</option>
+            <option value="">Today</option>
+            <option value="">Last Month</option>
+            <option value="">Quarterly</option>
+          </select>
+          <script>
+            $("#select2Period").select2({
+              theme: "bootstrap-5",
+            });
+          </script>
+        </div>
+        <div class="col-auto">
+          <button type="submit" class="btn btn-primary btn-icon-text"><i data-feather="filter" class="icon-sm mb-1 btn-icon-prepend"></i>Filter Data</button>
+        </div>
+      </div>
+      <?= form_close() ?>
+    </div>
   </div>
 
   <div class="row">
@@ -46,19 +68,6 @@
             <div class="card-body">
               <div class="d-flex justify-content-between align-items-baseline mb-2">
                 <h6 class="card-title mb-0">Statistics</h6>
-                <div>
-                  <select name="" id="select2Period" class="form-control-sm">
-                    <option value="">-- Select Duration --</option>
-                    <option value="">Today</option>
-                    <option value="">Last Month</option>
-                    <option value="">Quarterly</option>
-                  </select>
-                  <script>
-                    $("#select2Period").select2({
-                      theme: "bootstrap-5",
-                    });
-                  </script>
-                </div>
               </div>
               <div class="bg-light p-3 mb-2">
                 <div class="row justify-content-between align-items-center">
@@ -111,8 +120,8 @@
                       <div class="btn btn-primary text-white p-2"><i data-feather="calendar" class="h1"></i></div>
                     </div>
                     <div class="col-lg col-12">
-                      <h5>Lorem, ipsum dolor. (<?= date('F j, Y') ?>)</h5>
-                      <p>Lorem ipsum dolor sit.</p>
+                      <h5>Lorem, ipsum dolor.</h5>
+                      <p>(<?= date('F j, Y') ?>)</p>
                     </div>
                     <div class="col-lg-auto col-12">
                       <a href="" class="btn btn-primary btn-icon-text">View All<i data-feather="arrow-right" class="icon-sm mb-1 btn-icon-append"></i></a>
@@ -133,17 +142,6 @@
               <div class="d-flex justify-content-between align-items-baseline mb-3">
                 <h6 class="card-title mb-0">Attendance & Leaves</h6>
                 <div>
-                  <select name="" id="select2LeavePeriod" class="">
-                    <option value="">-- Select Year --</option>
-                    <option value="">2024</option>
-                    <option value="">2023</option>
-                    <option value="">2022</option>
-                  </select>
-                  <script>
-                    $("#select2LeavePeriod").select2({
-                      theme: "bootstrap-5",
-                    });
-                  </script>
                 </div>
               </div>
               <div class="row g-3 mb-3">
@@ -195,19 +193,6 @@
             <div class="card-body">
               <div class="d-flex justify-content-between align-items-baseline mb-2">
                 <h6 class="card-title mb-0">Working Hours</h6>
-                <div>
-                  <select name="" id="select2GraphPeriod" class="form-control-sm">
-                    <option value="">-- Select Duration --</option>
-                    <option value="">Today</option>
-                    <option value="">Last Month</option>
-                    <option value="">Quarterly</option>
-                  </select>
-                  <script>
-                    $("#select2GraphPeriod").select2({
-                      theme: "bootstrap-5",
-                    });
-                  </script>
-                </div>
               </div>
               <div class="" style="height: 200px;"></div>
             </div>
@@ -334,71 +319,6 @@
               },
             });
           </script>
-        </div>
-      </div>
-    </div>
-  </div> <!-- row -->
-
-  <div class="row">
-    <div class="col-lg-7 col-xl-9 grid-margin stretch-card">
-      <div class="card">
-        <div class="card-body">
-          <div class="d-flex justify-content-between align-items-baseline mb-2">
-            <h6 class="card-title mb-0">Monthly sales</h6>
-            <div class="dropdown mb-2">
-              <a type="button" id="dropdownMenuButton4" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <i class="icon-lg text-muted pb-3px" data-feather="more-horizontal"></i>
-              </a>
-              <div class="dropdown-menu" aria-labelledby="dropdownMenuButton4">
-                <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i data-feather="eye" class="icon-sm me-2"></i> <span class="">View</span></a>
-                <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i data-feather="edit-2" class="icon-sm me-2"></i> <span class="">Edit</span></a>
-                <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i data-feather="trash" class="icon-sm me-2"></i> <span class="">Delete</span></a>
-                <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i data-feather="printer" class="icon-sm me-2"></i> <span class="">Print</span></a>
-                <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i data-feather="download" class="icon-sm me-2"></i> <span class="">Download</span></a>
-              </div>
-            </div>
-          </div>
-          <p class="text-muted">Sales are activities related to selling or the number of goods or services sold in a given time period.</p>
-          <div id="monthlySalesChart"></div>
-        </div>
-      </div>
-    </div>
-    <div class="col-lg-5 col-xl-3 grid-margin stretch-card">
-      <div class="card">
-        <div class="card-body">
-          <div class="d-flex justify-content-between align-items-baseline">
-            <h6 class="card-title mb-0">Cloud storage</h6>
-            <div class="dropdown mb-2">
-              <a type="button" id="dropdownMenuButton5" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <i class="icon-lg text-muted pb-3px" data-feather="more-horizontal"></i>
-              </a>
-              <div class="dropdown-menu" aria-labelledby="dropdownMenuButton5">
-                <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i data-feather="eye" class="icon-sm me-2"></i> <span class="">View</span></a>
-                <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i data-feather="edit-2" class="icon-sm me-2"></i> <span class="">Edit</span></a>
-                <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i data-feather="trash" class="icon-sm me-2"></i> <span class="">Delete</span></a>
-                <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i data-feather="printer" class="icon-sm me-2"></i> <span class="">Print</span></a>
-                <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i data-feather="download" class="icon-sm me-2"></i> <span class="">Download</span></a>
-              </div>
-            </div>
-          </div>
-          <div id="storageChart"></div>
-          <div class="row mb-3">
-            <div class="col-6 d-flex justify-content-end">
-              <div>
-                <label class="d-flex align-items-center justify-content-end tx-10 text-uppercase fw-bolder">Total storage <span class="p-1 ms-1 rounded-circle bg-secondary"></span></label>
-                <h5 class="fw-bolder mb-0 text-end">8TB</h5>
-              </div>
-            </div>
-            <div class="col-6">
-              <div>
-                <label class="d-flex align-items-center tx-10 text-uppercase fw-bolder"><span class="p-1 me-1 rounded-circle bg-primary"></span> Used storage</label>
-                <h5 class="fw-bolder mb-0">~5TB</h5>
-              </div>
-            </div>
-          </div>
-          <div class="d-grid">
-            <button class="btn btn-primary">Upgrade storage</button>
-          </div>
         </div>
       </div>
     </div>
